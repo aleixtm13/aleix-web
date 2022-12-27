04195a7b18cebe3f2b02cd048c4966bfec0dc8ee
aleixtm13 <aleix13t13m@gmail.com>
2022-12-27 16:38:42 +0100

    Updated navbar styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,19 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <header className="text-gray-400 bg-orange-400 body-font">
-            <nav>
-            <ul>
-                <li><Link to="/events">Esdeveniments</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contacte</Link></li>
-            </ul>
-            </nav>
+        <header className="bg-orange-400 md:sticky top-0 z-10">
+            <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
+                <a className='title-font font-medium text-white mb-4 md:mb-0'>
+                    <Link to="/about" className='ml-3 text-xl'>Xest</Link>
+                </a>
+                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+                    <ul>
+                        <li><Link to="/about" className="mr-5 hover:text-white">Sobre</Link></li>
+                        <li><Link to="/contact" className="mr-5 hover:text-white">Contacte</Link></li>
+                    </ul>
+                </nav>
+            </div>
+           
       </header>
     );
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -57,6 +57,7 @@ const Contact = () => {
                 name="contact"
                 onSubmit={handleSubmit}
                 className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                      <input type="hidden" name="form-name" value="contact" />
                       <p className="leading-relaxed mb-5">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
                         suscipit officia aspernatur veritatis. Asperiores, aliquid?
