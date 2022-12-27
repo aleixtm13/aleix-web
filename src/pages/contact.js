505e1b943dceaee18beed105f499106bd868d21e
aleixtm13 <aleix13t13m@gmail.com>
2022-12-27 21:41:16 +0100

    updated contact form styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -71,7 +71,7 @@ const Contact = () => {
                         type="text"
                         id="name"
                         name="name"
-                        className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        className="w-full bg-white rounded border border-orange-400 focus:border-orange-400 focus:ring-2 focus:ring-orange-400 text-base outline-none text-black py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                         />
                     </div>
                     <div className="relative mb-4">
@@ -83,7 +83,7 @@ const Contact = () => {
                         type="email"
                         id="email"
                         name="email"
-                        className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        className="w-full bg-white rounded border border-orange-400 focus:border-orange-400 focus:ring-2 focus:ring-orange-400 text-base outline-none text-black py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                         />
                     </div>
                     <div className="relative mb-4">
@@ -96,12 +96,12 @@ const Contact = () => {
                          onChange={(e) => setMessage(e.target.value)}
                         id="message"
                         name="message"
-                        className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                        className="w-full bg-white rounded border border-orange-400 focus:border-orange-400 focus:ring-2 focus:ring-orange-4a00 h-32 text-base outline-none text-black-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                         />
                     </div>
                     <button
                         type="submit"
-                        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                        className="text-white bg-orange-400 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                         Envia
                     </button>
                 </form>
