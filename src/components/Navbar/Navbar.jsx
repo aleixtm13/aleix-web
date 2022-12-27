d540510a6d336d114dc8802e5e4252257e18864d
aleixtm13 <aleix13t13m@gmail.com>
2022-12-27 16:39:53 +0100

    fixed warning

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <header className="bg-orange-400 md:sticky top-0 z-10">
             <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
-                <a className='title-font font-medium text-white mb-4 md:mb-0'>
+                <a className='title-font font-medium text-white mb-4 md:mb-0' href='/'>
                     <Link to="/about" className='ml-3 text-xl'>Xest</Link>
                 </a>
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
